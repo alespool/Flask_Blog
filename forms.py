a81985896759c73cd25847ccd3a06dee6f7ab89c
alespool <alessionespoli.97@gmail.com>
2024-12-17 08:38:02 +0100

    Fixed bugs

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', 
 		validators=[DataRequired(), Length(min=2,max=20)])
@@ -9,12 +10,12 @@ class RegistrationForm(FlaskForm):
 		validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password', 
-		validators=[DataRequired(), EqualTo('pasword')])
+		validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', 
 		validators=[DataRequired(), Email()])
-	username = PasswordField('Password', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
