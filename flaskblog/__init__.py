1c856ea1de336c470126f06fd3a666605225022e
alespool <alessionespoli.97@gmail.com>
2024-12-23 15:38:54 +0100

    Added blueprints and configuration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,26 +1,20 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-# from authlib.integrations.flask_client import OAuth
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f2f3141974a58563486abc36edd3fcff'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER']='live.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-mail = Mail(app)
+
+mail = Mail()
+
+# from authlib.integrations.flask_client import OAuth
 
 # OAuth Configurations
 # app.config['GOOGLE_CLIENT_ID'] = os.environ.get('GOOGLE_CLIENT_ID')
@@ -39,4 +33,23 @@ mail = Mail(app)
 #     },
 # )
 
-from flaskblog import routes
+
+def create_app(config_class=Config):
+    
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
